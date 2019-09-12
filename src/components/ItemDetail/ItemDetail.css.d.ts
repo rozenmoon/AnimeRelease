d373a1520fa03ff9917d6b6aa163a0d7ff1524ca
rozenmoon <rozensmoon@gmail.com>
2019-09-12 14:27:11 +0900

    add item detail layout

---
+++ src/components/ItemDetail/ItemDetail.css.d.ts
@@ -0,0 +1,15 @@
+export const container: string;
+export const background: string;
+export const animePoster: string;
+export const details: string;
+export const animePosterHolder: string;
+export const menu: string;
+
+export const animeHeader: string;
+export const title: string;
+export const releaseDate: string;
+export const description: string;
+export const genres: string;
+export const genre: string;
+export const navigation: string;
+export const category: string;
--- src/components/ItemDetail/ItemDetail.tsx
+++ src/components/ItemDetail/ItemDetail.tsx
@@ -1,20 +1,80 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import * as styles from "./ItemDetail.css";
 
 interface Props {
-	SelectedAnime: { selectedAnime: number, SelectedAnimeData: object};
+  SelectedAnime: { selectedAnime: number; selecedAnimeData: any };
 }
 
-const ItemDetail :React.SFC<Props> = (props: Props) => {
-	return(
-		<div>
-			{props.SelectedAnime.selectedAnime}
-		</div>
-	);
+enum Sections{
+	Overview = 1,
+	Details
 }
 
-const mapStateToProps = (state) => ({
- SelectedAnime: state.SelectedAnime
+interface State {
+	currentSelected: Sections;
+}
+
+class ItemDetail extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			currentSelected : Sections.Overview
+		}
+	}
+
+	public render() {
+  if (this.props.SelectedAnime.selecedAnimeData) {
+    let {
+      image_url,
+      title_japanese,
+      synopsis,
+      genres,
+      aired
+    } = this.props.SelectedAnime.selecedAnimeData;
+    return (
+      <div className={styles.container}>
+        <div className={styles.background}>
+          <span className={styles.animePosterHolder}>
+            <img
+              className={styles.animePoster}
+              src={image_url ? image_url : null}
+            ></img>
+          </span>
+        </div>
+        <div className={styles.details}>
+          <div className={styles.animeHeader}>
+            <div>
+              <p className={styles.title}>{title_japanese}</p>
+              <div className={styles.releaseDate}>
+                {"Release Date: "}
+                {aired.from ? aired.from : "To be decided"}
+              </div>
+              <div className={styles.genres}>
+                {genres.map(genre => (
+                  <div className={styles.genre}>{genre.name}</div>
+                ))}
+              </div>
+              <div className={styles.description}>{synopsis}</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.menu}>
+          <div className={styles.navigation}>
+            <div className={styles.category}>{"Overview"}</div>
+            <div className={styles.category}>{"Details"}</div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+	}
+}
+};
+
+const mapStateToProps = state => ({
+  SelectedAnime: state.SelectedAnime
 });
 
 export default connect(mapStateToProps)(ItemDetail);
